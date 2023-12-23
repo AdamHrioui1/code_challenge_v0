a91c0cebe1fc230c078dbb4d6e4574ac5d677400
adamhrioui1 <essaouira024@gmail.com>
2023-12-23 20:07:29 +0100

    second commit

--- server/server.js
+++ server/server.js
@@ -6,11 +6,12 @@ const cors = require('cors')
 const app = express()
 let port = process.env.PORT || 8080
 
-app.use(cors({
-    origin: [''],
-    methods: ['POST', 'GET'],
-    credentials: true
-}))
+app.use(cors())
+// app.use(cors({
+//     origin: [''],
+//     methods: ['POST', 'GET'],
+//     credentials: true
+// }))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
